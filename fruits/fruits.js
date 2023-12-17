b7d74c2a56a9d3126d4f4b0f2959b673067a12bd
laxman-dhami <dhamilaxman555@gmail.com>
2023-12-17 13:10:00 +0530

    commit3

--- fruits/fruits.js
+++
@@ -1,6 +0,0 @@
-const fruit=document.getElementsByClassName('fruit')
-fruit[2].style.backgroundColor='yellow'
-fruit[0].style.fontWeight="bold"
-fruit[1].style.fontWeight="bold"
-fruit[2].style.fontWeight="bold"
-fruit[3].style.fontWeight="bold"
